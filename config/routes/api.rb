cd5d605b3cd5c13df4c25260a8dff1b339c58154
Andy Brett <andy@andybrett.com>
2021-02-19 21:43:42 -0800

    redirect current routes to api/legacy/*

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -78,5 +78,7 @@ Openfoodnetwork::Application.routes.draw do
         end
       end
     end
+
+    match '*path', to: redirect("/api/legacy/%{path}"), via: :all, constraints: { path: /(?!.*legacy).+/ }
   end
 end
