f381ef91aabe788dfcabdc3aad7df529a761e463
Andy Brett <andy@andybrett.com>
2021-03-05 14:44:40 -0800

    Move all existing api routes under /api/legacy

---
+++ app/controllers/api/legacy/logos_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Api
+  module Legacy
+    class LogosController < Api::Legacy::EnterpriseAttachmentController
+      private
+
+      def attachment_name
+        :logo
+      end
+
+      def enterprise_authorize_action
+        case action_name.to_sym
+        when :destroy
+          :remove_logo
+        end
+      end
+    end
+  end
+end
--- engines/web/config/routes.rb
+++ engines/web/config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Openfoodnetwork::Application.routes.append do
-  scope '/api/cookies' do
-    resource :consent, only: [:show, :create, :destroy], controller: "web/api/cookies_consent"
+  scope '/api/legacy/cookies' do
+    resource :consent, only: [:show, :create, :destroy], controller: "web/api/legacy/cookies_consent"
   end
 
   get "/angular-templates/:id", to: "web/angular_templates#show", constraints: { name: %r{[\/\w\.]+} }
